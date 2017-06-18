5b9e579b9fc71a050530e6365319308a44461497
DRL <drl.aivik@gmail.com>
2017-06-18 22:07:30 +0300

    qqq

--- scripts/drl_tmp/qqq.py
+++ scripts/drl_tmp/qqq.py
@@ -89,3 +89,108 @@ reg.DeleteKeyEx(root, subs[0])
 pp([
 	reg.DeleteKeyEx(root, k) for k in subs
 ])
+
+
+
+
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+t = np.arange(0.0, 2.0, 0.01)
+t = [x * 0.1 for x in xrange(10)]
+s = [(x * 13) % 3.0 for x in t]
+s = 1 + np.sin(2*np.pi*t)
+plt.plot(t, s)
+
+plt.xlabel('time (s)')
+plt.ylabel('voltage (mV)')
+plt.title('About as simple as it gets, folks')
+plt.grid(True)
+plt.savefig("test.png")
+plt.show()
+
+from os import path
+path.abspath('.')
+
+
+import mining_graph as m
+reload(m)
+from pprint import pprint as pp
+from datetime import timedelta as td
+pp(m.parse_log_files(hash_rate=True, gpu=False))
+m.display_graph(max_timedelta=td(days=1, hours=0), hash_rate=True, gpu_temp=True, gpu_fan=True)
+m.display_graph(hash_rate=True, gpu_temp=True, gpu_fan=True)
+
+
+a = '215.4'
+float(a)
+
+"""
+Compute the coherence of two signals
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+# make a little extra space between the subplots
+plt.subplots_adjust(wspace=0.5)
+
+dt = 0.01
+t = np.arange(0, 30, dt)
+nse1 = np.random.randn(len(t))                 # white noise 1
+nse2 = np.random.randn(len(t))                 # white noise 2
+r = np.exp(-t/0.05)
+
+cnse1 = np.convolve(nse1, r, mode='same')*dt   # colored noise 1
+cnse2 = np.convolve(nse2, r, mode='same')*dt   # colored noise 2
+
+# two signals with a coherent part and a random part
+s1 = 0.01*np.sin(2*np.pi*10*t) + cnse1
+s2 = 0.01*np.sin(2*np.pi*10*t) + cnse2
+
+plt.subplot(311)
+plt.plot(t, s1, t, s2)
+plt.xlim(0, 5)
+plt.xlabel('time')
+plt.ylabel('s1 and s2')
+plt.grid(True)
+
+plt.subplot(312)
+cxy, f = plt.cohere(s1, s2, 256, 1./dt)
+plt.ylabel('coherence')
+plt.show()
+plt.ticklabel_format
+
+
+
+
+"""
+===============================
+Legend using pre-defined labels
+===============================
+
+Notice how the legend labels are defined with the plots!
+"""
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Make some fake data.
+a = b = np.arange(0, 3, .02)
+c = np.exp(a)
+d = c[::-1]
+
+# Create plots with pre-defined labels.
+fig, ax = plt.subplots()
+ax.plot(a, c, 'k--', label='Model length')
+ax.plot(a, d, 'k:', label='Data length')
+ax.plot(a, c + d, 'k', label='Total message length')
+
+legend = ax.legend(loc='upper center', shadow=True, fontsize='x-large')
+
+# Put a nicer background color on the legend.
+legend.get_frame().set_facecolor('#00FFCC')
+
+plt.show()
