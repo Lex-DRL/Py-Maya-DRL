12f551aec8d72fddf4361955f4f9112c7f7d572b
DRL <drl.aivik@gmail.com>
2017-09-01 22:50:14 +0300

    user buttons

--- scripts/drl_user_buttons/aivik.py
+++ scripts/drl_user_buttons/aivik.py
@@ -10,3 +10,11 @@ def update_duplicated_meshes():
 
 def update_duplicated_with_offset():
 	return _avk.update_duplicated.meshes_and_transforms()
+
+
+def export_buildings(map1_res=2048):
+	return _avk.export.Buildings().export(map1_res=map1_res)
+
+
+def export_pve_islands(map1_res=2048):
+	return _avk.export.IslandsPVE().export(map1_res=map1_res)
