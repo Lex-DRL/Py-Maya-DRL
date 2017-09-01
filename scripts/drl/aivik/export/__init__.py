3d639094a92903446689f51cd808ac4973f5b72c
DRL <drl.aivik@gmail.com>
2017-09-01 18:27:57 +0300

    started "Buildings" exporter

--- scripts/drl/aivik/export/__init__.py
+++ scripts/drl/aivik/export/__init__.py
@@ -358,6 +358,45 @@ class BaseExport(object):
 		return self
 
 
+class Buildings(BaseExport):
+	_kept_colors_name_starts = (
+		"ground",
+		"hill",
+		"island",
+		"vane",
+		"steam"
+	)
+	_kept_colors_name_starts_f = tuple(
+		re.compile(
+			r'^{0}[\d_A-Za-z]*$'.format(x.lower())
+		)
+		for x in _kept_colors_name_starts
+	)
+
+	@staticmethod
+	def do_keep_color(obj):
+		"""
+		Checks given object name and tells if the object
+		should keep it's vertex-color during cleanup.
+
+		:param obj: <str / PyNode>
+		:return: <bool>
+		"""
+		nm_lower = ls.short_item_name(obj).lower()
+		nm_lower = nm_lower.split('.')[0]  # remove component to get a shape name
+		return any(
+			f.match(nm_lower) for f in Buildings._kept_colors_name_starts_f
+		)
+
+	def color_sets_cleanup(self):
+		"""
+		Removes color sets for anything with not matching name.
+		Matching names are listed in _kept_colors_name_starts.
+		"""
+		self._color_sets_cleanup(Buildings.do_keep_color)
+		return self
+
+
 _island_match_trees = re.compile(r'^([\d_A-Za-z]+)_Trees\d*$')
 _island_match_enemy_base = re.compile(r'.*enemy[\dA-Za-z]*base.*')
 _island_kept_colors_name_parts = (
