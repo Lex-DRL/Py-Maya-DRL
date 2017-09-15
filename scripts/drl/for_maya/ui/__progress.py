d84f18cc107d045c809319142361ba8686f84f39
DRL <drl.aivik@gmail.com>
2017-09-15 21:56:43 +0300

    rem unnecessary todo

--- scripts/drl/for_maya/ui/__progress.py
+++ scripts/drl/for_maya/ui/__progress.py
@@ -250,7 +250,7 @@ class Progress(object):
 		self.__max_displayed = 3
 		self.__max_displayed = int(max_displayed)
 		self.__width = int(width)
-		self._width_can_change = False  # TODO: update if parent has it enabled + _gen_update_f
+		self._width_can_change = False
 		self.__id = None  # type: Optional[str]
 		self.__set_id(id)  # can be here, since it doesn't depend on anything
 
