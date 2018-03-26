b8f6fc376a0a203882aaa5fff7c1d03414841a06
DRL <drl.aivik@gmail.com>
2018-03-26 22:33:57 +0300

    testing

--- scripts/drl_tmp/qqq.py
+++ scripts/drl_tmp/qqq.py
@@ -14,3 +14,17 @@ reload(fbx)
 fbx.FBX().presets_path()
 exporter = fbx.BatchExporter(r'e:\1-Projects\0-Common_Code')
 exporter.load_preset('AIVIK-Geo').add_selected_groups().export_all_groups()
+
+
+from drl_common.srgb import linear_to_srgb
+linear_to_srgb(0.5)
+
+
+from drl.for_maya.ls.convert.components import Poly
+Poly().to_vertex_faces()
+
+from drl.for_maya.auto.cleanup import UVSetsRule as Rule
+Rule('LM_color').kept_sets_for_object()
+
+from drl.for_maya.auto import BakedToUVs
+BakedToUVs().transfer()
