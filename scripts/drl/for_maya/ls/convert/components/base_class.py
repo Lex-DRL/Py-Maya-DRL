07f511b69252732caafbf6c06d54a208a22aa600
DRL <drl.aivik@gmail.com>
2019-05-06 20:05:23 +0300

    ls.convert.Poly(): type hints for all the methods + a bugfix for vertex-face grouping

--- scripts/drl/for_maya/ls/convert/components/base_class.py
+++ scripts/drl/for_maya/ls/convert/components/base_class.py
@@ -1,13 +1,16 @@
 __author__ = 'DRL'
 
 from functools import partial as _part
-
 from pymel import core as _pm
-
 from drl_common.utils import group_items as _group_items
-
 from drl.for_maya.base_class import PolyItemsProcessorBase as __BaseProcessor
 
+try:
+	# support type hints in Python 3:
+	import typing as _t
+except ImportError:
+	pass
+from drl_common.py_2_3 import str_t as _str_t, str_hint as _str_hint
 
 _flatten_f = _part(_pm.ls, fl=1)
 
@@ -17,16 +20,12 @@ def __get_vf_id_single_shape(vertex_face, dimension_id=0):
 	Gets the id of either vertex or face this vertex-face belongs to.
 
 	:param dimension_id:
-		<int>
-
 		* 0 - vertex
 		* 1 - face
-	:return:
-		<int>
-
-		id of a vertex/face
+	:return: id of a vertex/face
 	"""
-	return vertex_face.currentItemIndex()[dimension_id]
+	res = vertex_face.currentItemIndex()[dimension_id]  # type: int
+	return res
 
 
 def __get_vf_id_multi_shape(vertex_face, dimension_id=0):
@@ -34,17 +33,16 @@ def __get_vf_id_multi_shape(vertex_face, dimension_id=0):
 	Gets the id of either vertex or face this vertex-face belongs to.
 
 	:param dimension_id:
-		<int>
-
 		* 0 - vertex
 		* 1 - face
 	:return:
-		<string>, like: "pSphereShape1.1"
+		String ID, like: "pSphereShape1.1"
 
 		Where number after dot is an id of a vertex/face.
 	"""
 	vf_id = vertex_face.currentItemIndex()[dimension_id]
-	return vertex_face.node().name() + '.' + str(vf_id)
+	res = vertex_face.node().name() + '.' + str(vf_id)  # type: _str_hint
+	return res
 
 
 def __vfs_grouped(vertex_faces_list, single_shape=False, dimension_id=0):
@@ -55,20 +53,24 @@ def __vfs_grouped(vertex_faces_list, single_shape=False, dimension_id=0):
 	Returns them grouped either by the vertex or face (depending on <dimension_id> arg).
 
 	:param single_shape:
-		<bool>
-
 		Set to True, if the input is guaranteed to belong to a single shape.
 		It will work a little faster.
 	:param dimension_id:
-		<int>
 		Group by:
-
-		* 0 - vertex
-		* 1 - face
-	:return: <list of tuples>
+			* 0 - vertex
+			* 1 - face
 	"""
-	get_id = __get_vf_id_single_shape if single_shape else __get_vf_id_multi_shape
-	return _group_items(vertex_faces_list, get_id)
+	def get_id_single(vf):
+		return __get_vf_id_single_shape(vf, dimension_id)
+
+	def get_id_multi(vf):
+		return __get_vf_id_multi_shape(vf, dimension_id)
+
+	res = _group_items(
+		vertex_faces_list,
+		key_f=get_id_single if single_shape else get_id_multi
+	)  # type: _t.List[_t.Tuple[_pm.MeshVertexFace, ...]]
+	return res
 
 
 def vfs_grouped_by_vertex(vertex_faces_list, single_shape=False):
@@ -118,7 +120,7 @@ def vfs_grouped_by_face(vertex_faces_list, single_shape=False):
 		It will work a little faster.
 	:return: <list of tuples>
 	"""
-	return __vfs_grouped(vertex_faces_list, single_shape, 0)  # 1 = face
+	return __vfs_grouped(vertex_faces_list, single_shape, 1)  # 1 = face
 
 
 class PolyCompConverter(__BaseProcessor):
@@ -192,7 +194,8 @@ class PolyCompConverter(__BaseProcessor):
 			items = self.items
 
 		if not items:
-			return list()
+			res = list()  # type: _t.List[_pm.Component]
+			return res
 
 		keys_values = dict(
 			border=border or bo,
@@ -209,31 +212,34 @@ class PolyCompConverter(__BaseProcessor):
 			toVertexFace=to_vertex_face or tvf
 		)
 
-		kw_args = dict()
+		kw_args = {
+			kw: True
+			for kw, v in keys_values.iteritems()
+			if v
+		}
 
-		for kw, v in keys_values.iteritems():
-			if v:
-				kw_args[kw] = True
-
-		res = _pm.polyListComponentConversion(items, **kw_args)
+		res = _pm.polyListComponentConversion(items, **kw_args)  # type: _t.List[_pm.Component]
 
 		if not res:
-			return list()
+			res = list()  # type: _t.List[_pm.Component]
+			return res
 
 		if flatten or fl:
-			return _flatten_f(res)  # it's already guaranteed to be PyNodes
+			# it's already guaranteed to be PyNodes
+			res = _flatten_f(res)  # type: _t.List[_pm.Component]
 
 		if not isinstance(res[0], _pm.PyNode):
 			# assume: if 1st isn't a PyNode - then all of them are
-			return map(_pm.PyNode, res)
+			res = map(_pm.PyNode, res)  # type: _t.List[_pm.Component]
 
 		return res
 
 	def to_edges(self, flatten=False, internal=False, border=False):
-		return self.convert(
+		res = self.convert(
 			flatten=flatten, internal=internal, border=border,
 			to_edge=True
-		)
+		)  # type: _t.List[_pm.MeshEdge]
+		return res
 
 	def to_edges_on_uv_border(self, internal=False, include_geo_border=False):
 		"""
@@ -244,9 +250,10 @@ class PolyCompConverter(__BaseProcessor):
 		edges = self.convert(
 			flatten=True, internal=internal,
 			to_edge=True
-		)
+		)  # type: _t.List[_pm.MeshEdge]
 		if not edges:
-			return list()
+			res_empty = list()  # type: _t.List[_pm.MeshEdge]
+			return res_empty
 
 		to_edge_uvs_f = _part(_pm.polyListComponentConversion, fromEdge=True, toUV=True)
 		is_on_uv_border = lambda x: len(_flatten_f(to_edge_uvs_f(x))) > 2
@@ -255,19 +262,22 @@ class PolyCompConverter(__BaseProcessor):
 		else:
 			is_border_edge = is_on_uv_border
 
-		return [e for e in edges if is_border_edge(e)]
+		res = [e for e in edges if is_border_edge(e)]  # type: _t.List[_pm.MeshEdge]
+		return res
 
 	def to_vertices(self, flatten=False, internal=False, border=False):
-		return self.convert(
+		res = self.convert(
 			flatten=flatten, internal=internal, border=border,
 			to_vertex=True
-		)
+		)  # type: _t.List[_pm.MeshVertex]
+		return res
 
 	def to_vertex_faces(self, flatten=False, internal=False, border=False):
-		return self.convert(
+		res = self.convert(
 			flatten=flatten, internal=internal, border=border,
 			to_vertex_face=True
-		)
+		)  # type: _t.List[_pm.MeshVertexFace]
+		return res
 
 	def to_vertex_faces_grouped_by_vertex(
 		self, internal=False, border=False
@@ -281,14 +291,8 @@ class PolyCompConverter(__BaseProcessor):
 		If you're absolutely sure that the provided items is already
 		a flattened list of PyNode vertex-faces, then you can use
 		a faster **vfs_grouped_by_vertex()** function instead.
-
-		:return:
-			<list of lists>
 		"""
-		vfs = self.convert(
-			flatten=True, internal=internal, border=border,
-			to_vertex_face=True
-		)
+		vfs = self.to_vertex_faces(flatten=True, internal=internal, border=border)
 		return vfs_grouped_by_vertex(vfs, single_shape=False)
 
 	def to_vertex_faces_grouped_by_face(
@@ -303,24 +307,20 @@ class PolyCompConverter(__BaseProcessor):
 		If you're absolutely sure that the provided items is already
 		a flattened list of PyNode vertex-faces, then you can use
 		a faster **vfs_grouped_by_face()** function instead.
-
-		:return:
-			<list of lists>
 		"""
-		vfs = self.convert(
-			flatten=True, internal=internal, border=border,
-			to_vertex_face=True
-		)
+		vfs = self.to_vertex_faces(flatten=True, internal=internal, border=border)
 		return vfs_grouped_by_face(vfs, single_shape=False)
 
 	def to_faces(self, flatten=False, internal=False, border=False):
-		return self.convert(
+		res = self.convert(
 			flatten=flatten, internal=internal, border=border,
 			to_face=True
-		)
+		)  # type: _t.List[_pm.MeshFace]
+		return res
 
 	def to_uvs(self, flatten=False, internal=False, border=False):
-		return self.convert(
+		res = self.convert(
 			flatten=flatten, internal=internal, border=border,
 			to_uv=True
-		)
+		)  # type: _t.List[_pm.MeshUV]
+		return res
