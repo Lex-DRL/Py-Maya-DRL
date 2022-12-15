5915559f61f4e872e7232dc918b8c9aa15dd672b
Lex Darlog (DRL) <drl.i3x@gmail.com>
2022-12-15 18:54:46 +0300

    fix import in aivik

--- scripts/drl_user_buttons/aivik.py
+++ scripts/drl_user_buttons/aivik.py
@@ -2,28 +2,32 @@ __author__ = 'Lex Darlog (DRL)'
 
 from drl_py23 import reload
 from drl import aivik as _avk
+from drl.aivik import (
+	replace_shapes as _replace_shapes,
+	export as _export
+)
 reload(_avk)
 
 
 def update_duplicated_meshes():
-	return _avk.replace_shapes.from_many_sources()
+	return _replace_shapes.from_many_sources()
 
 
 def update_duplicated_with_offset():
-	return _avk.replace_shapes.from_many_sources_with_offset()
+	return _replace_shapes.from_many_sources_with_offset()
 
 
 def replace_shapes_with():
-	return _avk.replace_shapes.source_to_targets()
+	return _replace_shapes.source_to_targets()
 
 
 def export_buildings(map1_res=2048, kept_colors_regexps=None):
-	return _avk.export.Buildings(save_scene_warning=False).export(
+	return _export.Buildings(save_scene_warning=False).export(
 		map1_res=map1_res, kept_colors_regexps=kept_colors_regexps
 	)
 
 
 def export_pve_islands(map1_res=2048):
-	return _avk.export.IslandsPVE(save_scene_warning=False).export(
+	return _export.IslandsPVE(save_scene_warning=False).export(
 		map1_res=map1_res
 	)
